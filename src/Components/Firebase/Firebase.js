df86c8de26754119054b7b56613c0ef641f59d15
alaminiium2019 <alamin.iium2017@gmail.com>
2020-10-18 10:28:32 +0600

    first commit

---
+++ src/Components/Firebase/Firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAaRUdVB3n6z2Gl0Lwx5-JwMrokmFYoPt0",
+    authDomain: "chatme-1f48d.firebaseapp.com",
+    databaseURL: "https://chatme-1f48d.firebaseio.com",
+    projectId: "chatme-1f48d",
+    storageBucket: "chatme-1f48d.appspot.com",
+    messagingSenderId: "454734669306",
+    appId: "1:454734669306:web:79dc2ae41acc2bb4103043",
+    measurementId: "G-1CXPM8T8LP"
+  };
+
+  export default firebaseConfig;
