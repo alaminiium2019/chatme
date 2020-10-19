e2ca9dd7fda7588fdbfb85298522334959432ab2
alaminiium2019 <alamin.iium2017@gmail.com>
2020-10-19 12:10:27 +0600

    added chatsidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,11 +1,37 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { ChatBubbleOutline, ChatBubbleOutlineOutlined, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react';
+import SidebarChat from '../SidebarChat/SidebarChat';
 import './Sidebar.css';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <h1>Hello sidebar</h1>
-            
+            <div className="sidebar_header">
+                <Avatar></Avatar>
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLarge></DonutLarge>
+                    </IconButton>
+                    <IconButton>
+                        <ChatBubbleOutline></ChatBubbleOutline>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert></MoreVert>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                <SearchOutlined/>                
+                <input placeholder="Search or start new chat" type="text"></input>
+                </div>
+            </div>
+            <div className="sidebar_chats">
+            <SidebarChat addNewChat></SidebarChat>
+            <SidebarChat></SidebarChat>
+
+            </div>
         </div>
     );
 };
